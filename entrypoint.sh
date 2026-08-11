291873515ea92faece3468cff4d339885d8b9826
Lipovics Martin <lipovicsmartin@gmail.com>
2026-08-11 20:01:07 +0200

    Replace DooD with DiD

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 set -e
 
+echo "Adding runner user.."
 if ! getent group runner >/dev/null; then
     groupadd -g "$PGID" runner
 fi
@@ -11,22 +12,23 @@ if ! id -u runner >/dev/null 2>&1; then
     useradd -u "$PUID" -g "$PGID" -m -s /bin/bash runner
 fi
 
-if [ -S /var/run/docker.sock ]; then
-    DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-    DOCKER_GROUP=$(getent group "$DOCKER_GID" | cut -d: -f1)
-    
-    if [ -z "$DOCKER_GROUP" ]; then
-        if getent group docker >/dev/null; then
-            groupmod -g "$DOCKER_GID" docker
-            DOCKER_GROUP="docker"
-        else
-            groupadd -g "$DOCKER_GID" dockersock
-            DOCKER_GROUP="dockersock"
-        fi
-    fi
-    
-    usermod -aG "$DOCKER_GROUP" runner
+if ! getent group docker >/dev/null; then
+    groupadd docker
 fi
+
+usermod -aG docker runner
+echo "Starting Docker daemon..."
+dockerd > /var/log/dockerd.log 2>&1 &
+
+timeout 30 sh -c 'until docker info >/dev/null 2>&1; do sleep 1; done'
+
+if ! docker info >/dev/null 2>&1; then
+    echo "Error while starting the docker daemon!"
+    cat /var/log/dockerd.log
+    exit 1
+fi
+
+echo "Docker daemon is running"
 
 chown -R runner:runner /app
 exec gosu runner "$@"
